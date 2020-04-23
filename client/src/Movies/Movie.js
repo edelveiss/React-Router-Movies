ef7f95a6cb609fcdabfd9cad1f74bfff7126619f
Tatiana Zhizhimontova <tatyana.zhi@gmail.com>
2020-04-22 18:51:43 -0700

    MovieCard is added

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
+import MovieCard from "./MovieCard";
 
 const Movie = (props) => {
   console.log("movie props: ", props);
@@ -48,25 +49,11 @@ const Movie = (props) => {
     return <div>Loading movie information...</div>;
   }
 
-  const { title, director, metascore, stars } = movie;
+  //const { title, director, metascore, stars } = movie;
   return (
     <div className="save-wrapper">
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
+      <MovieCard key={movie.id} movie={movie} />
 
-        {stars.map((star) => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
       <div onClick={() => saveMovie()} className="save-button">
         Save
       </div>
@@ -79,3 +66,20 @@ const Movie = (props) => {
 };
 
 export default Movie;
+
+// <div className="movie-card">
+//         <h2>{title}</h2>
+//         <div className="movie-director">
+//           Director: <em>{director}</em>
+//         </div>
+//         <div className="movie-metascore">
+//           Metascore: <strong>{metascore}</strong>
+//         </div>
+//         <h3>Actors</h3>
+
+//         {stars.map((star) => (
+//           <div key={star} className="movie-star">
+//             {star}
+//           </div>
+//         ))}
+//       </div>
--- client/src/Movies/SavedMovies.js
+++ client/src/Movies/SavedMovies.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import MovieDetails from "./MovieList";
+import MovieCard from "./MovieCard";
 
 const SavedMovies = (props) => {
   console.log("savedMoviesProps", props.savedList);
@@ -9,33 +9,33 @@ const SavedMovies = (props) => {
       {props.savedList.map((movie, index) => (
         <Link className="text-link" key={index} to={`/movies/${movie.id}`}>
           {console.log("movie saved1111 ", movie)}
-          <MovieDetails1 key={movie.id} movie={movie} />
+          <MovieCard key={movie.id} movie={movie} />
         </Link>
       ))}
     </div>
   );
 };
 
-function MovieDetails1({ movie }) {
-  const { title, director, metascore, stars } = movie;
-  return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map((star) => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-    </div>
-  );
-}
-
 export default SavedMovies;
+
+//function MovieDetails1({ movie }) {
+//   const { title, director, metascore, stars } = movie;
+//   return (
+//     <div className="movie-card">
+//       <h2>{title}</h2>
+//       <div className="movie-director">
+//         Director: <em>{director}</em>
+//       </div>
+//       <div className="movie-metascore">
+//         Metascore: <strong>{metascore}</strong>
+//       </div>
+//       <h3>Actors</h3>
+
+//       {stars.map((star) => (
+//         <div key={star} className="movie-star">
+//           {star}
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
